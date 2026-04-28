e6033119c006f25f4f79edc13718f2b5d7484335
Bruno Leonardo <bruno.lsilva1508@gmail.com>
2026-04-27 23:30:31 -0300

    Initial Commit

---
+++ PalindromeApp/PalindromeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalindromeAppApp.swift
+//  PalindromeApp
+//
+//  Created by Bruno Silva on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PalindromeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
